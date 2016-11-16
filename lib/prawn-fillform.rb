d5f67f84670806d9b168f91ddd923533a54b570d
David Bai <valcrist77@gmail.com>
2016-11-16 15:11:44 -0800

    Update prawn-fillform.rb

--- lib/prawn-fillform.rb
+++ lib/prawn-fillform.rb
@@ -207,7 +207,7 @@ module Prawn
 
     def acroform_fields
       acroform = {}
-      @state.pages.each_with_index do |page, i|
+      state.pages.each_with_index do |page, i|
         annots = deref(page.dictionary.data[:Annots])
         page_number = "page_#{i+1}".to_sym
         acroform[page_number] = []
